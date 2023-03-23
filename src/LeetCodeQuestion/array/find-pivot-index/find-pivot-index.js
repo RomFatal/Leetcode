6d6e8ae7a69bf844f27cad0207a9de99980e401a
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-03-23 23:52:39 +0200

    adding find-pivot-index

---
+++ src/LeetCodeQuestion/array/find-pivot-index/find-pivot-index.js
@@ -0,0 +1,36 @@
+//findPivotIndex Complexity:O(n) Space Complexity: O(1)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+export default function findPivotIndex (nums) {
+  let totalSum=0, right=0, left=0;
+  for(let i=0; i < nums.length; i++)
+      totalSum += nums[i];
+
+  left = totalSum;
+  for(let i=0; i < nums.length; i++)
+  { 
+      console.log(right ,left - nums[i])
+      if( right != left - nums[i] )
+      {
+          right += nums[i];
+          left -=nums[i];
+      }
+      else 
+          return i;
+  }
+  return -1;
+};
+
+
+
+
+
+
+
+
+
+
+
+
