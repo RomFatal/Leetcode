45ec19cf36d7ac2023d69e91622476879ebca492
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-03-23 05:33:54 +0200

    format

--- src/LeetCodeQuestion/best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ src/LeetCodeQuestion/best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -1,18 +1,17 @@
 //At most one transaction is allowed Time Complexity: O(N) Auxiliary Space: O(1)
-export function maxProfit( prices)
-{
+export function maxProfit(prices) {
     let buy = prices[0], max_profit = 0;
     let n = prices.length;
     for (let i = 1; i < n; i++) {
- 
+
         // Checking for lower buy value
         if (buy > prices[i])
             buy = prices[i];
- 
+
         // Checking for higher profit
         else if (prices[i] - buy > max_profit)
             max_profit = prices[i] - buy;
     }
     return max_profit;
 }
- 
+
