fdc3160bfaa4acee9cde6e2a93e2e2f4c1245631
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-04-04 04:29:52 +0300

    adding tim-sort

---
+++ src/LeetCodeQuestion/algorithm/sorting/tim-sort/tim-sort.js
@@ -0,0 +1,74 @@
+//Time Complexity: Ω(n) Θ(n log(n)) O(n log(n))  Auxiliary Space: O(n)
+
+const RUN = 2;
+
+function insertionSort(arr, left, right) {
+  for (let i = left + 1; i <= right; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+    while (j >= left && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+}
+
+function merge(arr, l, m, r) {
+  let len1 = m - l + 1,
+    len2 = r - m;
+  let left = new Array(len1);
+  let right = new Array(len2);
+
+  for (let x = 0; x < len1; x++) {
+    left[x] = arr[l + x];
+  }
+  for (let x = 0; x < len2; x++) {
+    right[x] = arr[m + 1 + x];
+  }
+
+  let i = 0;
+  let j = 0;
+  let k = l;
+
+  while (i < len1 && j < len2) {
+    if (left[i] <= right[j]) {
+      arr[k] = left[i];
+      i++;
+    } else {
+      arr[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < len1) {
+    arr[k] = left[i];
+    k++;
+    i++;
+  }
+
+  while (j < len2) {
+    arr[k] = right[j];
+    k++;
+    j++;
+  }
+}
+
+export default function timSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n; i += RUN) {
+    insertionSort(arr, i, Math.min(i + RUN - 1, n - 1));
+  }
+
+  for (let size = RUN; size < n; size = 2 * size) {
+    for (let left = 0; left < n; left += 2 * size) {
+      const mid = left + size - 1;
+      const right = Math.min(left + 2 * size - 1, n - 1);
+      merge(arr, left, mid, right);
+    }
+  }
+
+  return arr;
+}
